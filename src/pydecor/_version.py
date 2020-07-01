44a563053b01e55f9923ac4e7948c56db35d0ded
Matthew Planchard <msplanchard@gmail.com>
2020-07-01 15:00:24 -0500

    CHORE: bump patch version (2.0.1)

--- src/pydecor/_version.py
+++ src/pydecor/_version.py
@@ -8,5 +8,5 @@ and also set as the __version__ attribute for the package.
 
 from __future__ import absolute_import, unicode_literals
 
-__version_info__ = (2, 0, 0)
+__version_info__ = (2, 0, 1)
 __version__ = ".".join([str(ver) for ver in __version_info__])
